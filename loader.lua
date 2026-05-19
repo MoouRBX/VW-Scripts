8c33fc90835f9363c21e27e1e6c8677a7237e6ad
Moou <broisdummy920@gmail.com>
2026-05-19 19:13:14 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
 -- COPY THIS AND PASTE IT IN YOUR EXECUTOR 👇
-loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/loader2.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MoouRBX/VW-Add/main/loader2.lua", true))()
